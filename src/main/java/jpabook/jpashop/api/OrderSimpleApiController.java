e23337128212122750a17c5d065045d222a126d9
ljs3536 <wlstjr320@naver.com>
2024-02-28 22:40:49 +0900

    간단한 주문 조회 V1: 엔티티를 직접 노출 2

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -24,6 +24,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for(Order order : all){
+            order.getMember().getName(); //lazy 강제 초기화
+            order.getDelivery().getAddress(); //lazy 강제 초기화
+        }
         return all;
     }
 
