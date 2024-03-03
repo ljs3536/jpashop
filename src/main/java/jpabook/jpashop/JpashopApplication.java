d8d1a03bbadeddefe84767fbe4024ee7332059bf
ljs3536 <wlstjr320@naver.com>
2024-03-03 11:21:46 +0900

    주문 조회 V1: 엔티티 직접 노출

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,6 +1,6 @@
 package jpabook.jpashop;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,10 +17,10 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
-//	@Bean
-//	Hibernate5Module hibernate5Module(){
-//		Hibernate5Module hibernate5Module = new Hibernate5Module();
-//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
-//		return hibernate5Module;
-//	}
+	@Bean
+	Hibernate5JakartaModule hibernate5JakartaModule() {
+		Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+		//hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5JakartaModule;
+	}
 }
